8617cf14ab8277deed08b7d5a7e91c9a5a915b5b
Nguyen Trong Luat <55679058+luat111@users.noreply.github.com>
2020-10-22 21:07:40 +0700

    Update Payment.js

--- App/src/screens/Payment.js
+++ App/src/screens/Payment.js
@@ -8,17 +8,12 @@ import {
 } from 'react-native';
 import { WebView } from 'react-native-webview';
 import Header from '../components/HeaderComponent';
-const { width } = Dimensions.get('window').width;
-const { height } = Dimensions.get('window').height;
-const hideJS = `
-    function formatStyle(){ 
-        getElementByXpath("/html/body/pre").style.display="none";
-    };
-    formatStyle();`;
+
+
 class Payment extends Component {
     webviewRef = null;
     _isMounted = false;
-
+    alreadyInjected = true;
     constructor(props) {
         super(props);
         this.state = {
@@ -67,13 +62,16 @@ class Payment extends Component {
     }
     hideSpinner() {
         this.setState({ visible: false });
-    }
+    } 
     render() {
         if (this.state.isLoading) {
             return (
-                <LoadingView loading={this.state.isLoading}>
-                    <ActivityIndicator animating={true} size="large" color="#0000ff" style={{marginBottom:height/2,marginLeft:width/2}} />
-                </LoadingView>
+                <View style={{ flex: 1, flexDirection: "column" }}>
+                    <Header title="Thanh toán" />
+                    <View style={{ alignItems: "center" }}>
+                        <ActivityIndicator animating={true} size="large" color="#0000ff" style={{ marginTop: 300 }} />
+                    </View>
+                </View>
             );
         }
         else {
@@ -84,12 +82,17 @@ class Payment extends Component {
                         ref={ref => this.webviewRef = ref}
                         source={{ uri: this.state.url }}
                         style={styles.Webview}
-                        onLoad={() => { this.hideSpinner() }}
+
+                        javaScriptEnabledAndroid={true}
+                        javaScriptEnabled={true}
+
+                        onLoad={() => {this.hideSpinner();}}
+
                         startInLoadingState={true}
                         onNavigationStateChange={this.closeWebView} />
-                    {this.state.visible && (
-                        <View style={{alignItems:"center"}}>
-                            <ActivityIndicator animating={true} size="large" color="#0000ff" style={{marginBottom:300}} />
+                    {this.state.visible && (                       
+                        <View style={{ alignItems: "center" }}>
+                            <ActivityIndicator animating={true} size="large" color="#0000ff" style={{ marginBottom: 300 }} />
                         </View>
                     )}
                 </View>
@@ -99,7 +102,7 @@ class Payment extends Component {
 }
 const styles = StyleSheet.create({
     Webview: {
-        flex: 1, backgroundColor: "green"
+        flex: 1,
     },
     flexContainer: {
         flex: 1
