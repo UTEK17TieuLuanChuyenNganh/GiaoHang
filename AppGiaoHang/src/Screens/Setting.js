afb88ae11232ecafc5df0908c2f399ddb5ab1d90
Caoquyen1913 <55612587+Caoquyen1913@users.noreply.github.com>
2020-12-19 12:01:44 +0700

    Update Setting.js

--- AppGiaoHang/src/Screens/Setting.js
+++ AppGiaoHang/src/Screens/Setting.js
@@ -1,9 +1,24 @@
 import React, { Component } from 'react';
-import { View, Text, TouchableOpacity ,StyleSheet} from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 import Logo from '../Component/Logo';
+import { connect } from 'react-redux';
+import store from '../redux/store';
 class Setting extends Component {
   logoutclick() {
+    store.dispatch({
+      type: 'CLEARORDER',
+    })
+    store.dispatch({
+      type: 'CLEARCHUOI',
+    })
+    store.dispatch({
+      type: 'CLEARSTT',
+    })
+    store.dispatch({
+      type: 'CLEARUSER',
+    })
     this.props.navigation.replace('Login');
+
   }
   showmenu = () => {
     this.props.navigation.openDrawer();
@@ -24,14 +39,14 @@ class Setting extends Component {
   }
 }
 const styles = StyleSheet.create({
-  
+
   logost: {
-      position: "absolute",
-      left: 0,
-      right: 0,
-      top: 0,
-      height: 50,
+    position: "absolute",
+    left: 0,
+    right: 0,
+    top: 0,
+    height: 50,
   },
-  
+
 })
 export default Setting;
